e2569ea44bcaf73c850740861450a6d15ed958a3
lokhandeyash46-boop <lokhandeyash46@gmail.com>
2026-03-18 06:31:30 -1200

    Add task-3: Calculator

---
+++ Calculator-Task-3/calc.js
@@ -0,0 +1,42 @@
+let inputBox = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
+
+let string = '';
+
+buttons.forEach(element => {
+    element.addEventListener('click', (b) => {
+        if (b.target.innerText == '=') {
+           try{
+            string = String(eval(string));
+            inputBox.value =string;
+        } catch {
+            inputBox.value = "Error";
+            string = "";
+          }
+        }
+        else if (b.target.innerText == 'AC') {
+            string = "";
+            inputBox.value =string;
+        }
+        else if (b.target.innerText == 'DEL')
+            {
+            string = string.substring(0, string.length - 1);
+            inputBox.value =string;    
+        }
+        else if (b.target.id == 'plusminus') {
+           try { 
+            string = String(-eval(string));
+            inputBox.value = string;
+           } catch {
+            inputBox.value = "Error";
+            string = "";
+           }
+        }
+        else {
+            string += b.target.innerText;
+            inputBox.value = string;
+        }
+    });
+
+});  
+
